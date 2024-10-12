ae45ec3eed001a1666a4e6bfa4aa1cffa02a103f
hqmdokkai <155129956+hqmdokkai@users.noreply.github.com>
2024-10-12 17:01:18 +0700

    Add files via upload

---
+++ bin/pygamemini-1.0-debug-unaligned.apk.d
@@ -0,0 +1,21 @@
+/Volumes/Data/Laptrinh/App/pygame/pgs4a/bin/pygamemini-1.0-debug-unaligned.apk : \
+/Volumes/Data/Laptrinh/App/pygame/pgs4a/bin/pygamemini-1.0.ap_ \
+/Volumes/Data/Laptrinh/App/pygame/pgs4a/bin/classes.dex \
+/Volumes/Data/Laptrinh/App/pygame/pgs4a/libs/android-support-v4.jar \
+/Volumes/Data/Laptrinh/App/pygame/pgs4a/libs/arm64-v8a/libsdl.so \
+/Volumes/Data/Laptrinh/App/pygame/pgs4a/libs/arm64-v8a/libsdl_image.so \
+/Volumes/Data/Laptrinh/App/pygame/pgs4a/libs/arm64-v8a/libsdl_mixer.so \
+/Volumes/Data/Laptrinh/App/pygame/pgs4a/libs/arm64-v8a/libsdl_ttf.so \
+/Volumes/Data/Laptrinh/App/pygame/pgs4a/libs/arm64-v8a/libsdl_main.so \
+/Volumes/Data/Laptrinh/App/pygame/pgs4a/libs/arm64-v8a/libpymodules.so \
+/Volumes/Data/Laptrinh/App/pygame/pgs4a/libs/arm64-v8a/libpython2.7.so \
+/Volumes/Data/Laptrinh/App/pygame/pgs4a/libs/arm64-v8a/libapplication.so \
+/Volumes/Data/Laptrinh/App/pygame/pgs4a/libs/armeabi/libsdl.so \
+/Volumes/Data/Laptrinh/App/pygame/pgs4a/libs/armeabi/libsdl_image.so \
+/Volumes/Data/Laptrinh/App/pygame/pgs4a/libs/armeabi/libsdl_mixer.so \
+/Volumes/Data/Laptrinh/App/pygame/pgs4a/libs/armeabi/libsdl_ttf.so \
+/Volumes/Data/Laptrinh/App/pygame/pgs4a/libs/armeabi/libsdl_main.so \
+/Volumes/Data/Laptrinh/App/pygame/pgs4a/libs/armeabi/libsqlite3.so \
+/Volumes/Data/Laptrinh/App/pygame/pgs4a/libs/armeabi/libpymodules.so \
+/Volumes/Data/Laptrinh/App/pygame/pgs4a/libs/armeabi/libpython2.7.so \
+/Volumes/Data/Laptrinh/App/pygame/pgs4a/libs/armeabi/libapplication.so \
